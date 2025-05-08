5f6c61051d95100b27c470d08f8a1d137d8bba8d
phthalos <phthalos64@gmail.com>
2025-05-08 11:34:43 +0900

    결과페이지 페이지네이션 추가

--- app/(with-background)/result/page.tsx
+++ app/(with-background)/result/page.tsx
@@ -8,11 +8,21 @@ import {
     DialogTitle,
     DialogTrigger,
 } from "@/components/ui/dialog";
+import {
+    Pagination,
+    PaginationContent,
+    PaginationEllipsis,
+    PaginationItem,
+    PaginationLink,
+    PaginationNext,
+    PaginationPrevious,
+} from "@/components/ui/pagination";
 
 export default function Result() {
     return (
         <div className="w-6/12">
             <h2 className="-mt-10 mb-8">테스트 결과</h2>
+            {/* 게임 목록 1개 */}
             <Dialog>
                 <DialogTrigger>
                     <ResultItem />
@@ -26,6 +36,31 @@ export default function Result() {
                     <DialogFooter>(팝업 footer 영역)</DialogFooter>
                 </DialogContent>
             </Dialog>
+            {/* 페이지네이션 */}
+            <Pagination>
+                <PaginationContent>
+                    <PaginationItem>
+                        <PaginationPrevious href="#" />
+                    </PaginationItem>
+                    <PaginationItem>
+                        <PaginationLink href="#">1</PaginationLink>
+                    </PaginationItem>
+                    <PaginationItem>
+                        <PaginationLink href="#" isActive>
+                            2
+                        </PaginationLink>
+                    </PaginationItem>
+                    <PaginationItem>
+                        <PaginationLink href="#">3</PaginationLink>
+                    </PaginationItem>
+                    <PaginationItem>
+                        <PaginationEllipsis />
+                    </PaginationItem>
+                    <PaginationItem>
+                        <PaginationNext href="#" />
+                    </PaginationItem>
+                </PaginationContent>
+            </Pagination>
         </div>
     );
 }
