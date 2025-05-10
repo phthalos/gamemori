43889f7922d3646a72690d83be17acaae743d81f
sojeong0302 <01duqrl@naver.com>
2025-05-10 17:10:58 +0900

    버튼 클릭 시 결과 페이지로 이동

--- app/(with-background)/personalize/page.tsx
+++ app/(with-background)/personalize/page.tsx
@@ -15,13 +15,13 @@ export default function Personalize() {
         // 유효성 검사 구현
     };
     return (
-        <div className='w-6/12'>
+        <div className="w-6/12">
             {/* 질문 & 답변 목록 */}
-            <form onSubmit={(e) => onSubmit(e)} name='personalizeform' action='/result' method='post'>
-                <ul className='flex flex-col gap-6 mb-5'>
+            <form onSubmit={(e) => onSubmit(e)} name="personalizeform" action="/result" method="post">
+                <ul className="flex flex-col gap-6 mb-5">
                     {personalizelist.map((value, index) => (
                         <li key={index}>
-                            <Card className='bg-violet-50/5'>
+                            <Card className="bg-violet-50/5">
                                 {/* 질문 */}
                                 <CardHeader>
                                     <CardTitle>
@@ -37,13 +37,13 @@ export default function Personalize() {
                                                 {/* 슬라이더 값 표시 구현 필요 */}
                                                 <Label htmlFor={index.toString()}>2025년</Label>
                                                 {/* 슬라이더 핸들 2개 구현 필요 */}
-                                                <Slider className='mt-3' defaultValue={[value.slider.defaultvalue]} />
+                                                <Slider className="mt-3" defaultValue={[value.slider.defaultvalue]} />
                                             </>
                                         ) : // 체크박스
                                         value.checkbox ? (
-                                            <ul className='flex flex-col gap-2'>
+                                            <ul className="flex flex-col gap-2">
                                                 {value.checkbox.map((item, checkboxindex) => (
-                                                    <div key={checkboxindex} className='flex items-center space-x-2'>
+                                                    <div key={checkboxindex} className="flex items-center space-x-2">
                                                         <Checkbox id={"checkbox" + checkboxindex.toString()} />
                                                         <Label htmlFor={"checkbox" + checkboxindex.toString()}>
                                                             {item.name}
@@ -55,7 +55,7 @@ export default function Personalize() {
                                         value.radio ? (
                                             <RadioGroup>
                                                 {value.radio.map((item, radioindex) => (
-                                                    <div key={radioindex} className='flex items-center space-x-2'>
+                                                    <div key={radioindex} className="flex items-center space-x-2">
                                                         <RadioGroupItem
                                                             value={item.name}
                                                             id={"radio" + radioindex.toString()}
@@ -78,8 +78,8 @@ export default function Personalize() {
                     ))}
                 </ul>
                 {/* 버튼 디자인 필요 */}
-                <Button type='submit' className='float-right'>
-                    결과 보러가기
+                <Button href="/result" type="submit" className="float-right">
+                    결과 보기
                 </Button>
             </form>
         </div>
