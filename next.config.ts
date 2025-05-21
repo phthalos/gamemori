37cd6cde65f3c48e2593126561179a683d5162ce
phthalos <phthalos64@gmail.com>
2025-05-21 14:32:20 +0900

    (주석처리)정적 사이트 생성 옵션 추가

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     /* config options here */
+    // output: "export", // 정적 사이트 생성
     images: {
         remotePatterns: [new URL("https://media.rawg.io/media/games/**")],
     },
