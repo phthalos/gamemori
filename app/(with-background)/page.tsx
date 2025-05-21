5976c624934791ea5516f07236c453d6917afeeb
phthalos <phthalos64@gmail.com>
2025-05-22 04:18:11 +0900

    메인페이지 50만개 게임.. 버튼 추가

--- app/(with-background)/page.tsx
+++ app/(with-background)/page.tsx
@@ -1,22 +1,27 @@
-// "use client";
-// import CrtModelViewer from "@/components/3d/crt";
 import ThumbnailMarqueeList from "@/components/common/thumbnail-marquee-list";
 import { ArrowRightIcon } from "@/components/ui/arrow-right";
+import { Button } from "@/components/ui/button";
 import ButtonStart from "@/components/ui/button-start";
 
 export default function Home() {
     return (
         <>
+            <Button variant="outline" size="sm" className="z-5 rounded-full text-neutral-200 mb-2 flex gap-1">
+                <span className="relative flex size-2">
+                    <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-violet-100 opacity-75" />
+                    <span className="relative inline-flex size-2 rounded-full bg-violet-300" />
+                </span>
+                +50만개 게임 데이터 제공
+            </Button>
+            {/* <span className="text-neutral-200 border rounded-full px-3 py-1 text-sm"> +50만개 게임 데이터 제공</span> */}
             <p
-                className={`text-6xl font-semibold bg-linear-120 from-violet-50 via-violet-300 via-70% to-violet-400 text-transparent bg-clip-text mb-6 leading-16 text-center 
-                ${/*text-shadow-[0_0px_6px] text-shadow-violet-50/60*/ ``}
-                `}
+                className={`text-5xl font-semibold bg-linear-120 from-violet-50 via-violet-300 via-70% to-violet-400 text-transparent bg-clip-text mb-6 leading-14 text-center`}
             >
                 게임의 우주에서
                 <br />
                 <span className="font-[SokchoBadaBatang] tracking-tighter">숨겨진 보석</span>을 찾아보세요
             </p>
-            <p className="mb-10 relative opacity-75 text-center">
+            <p className="mb-8 relative opacity-75 text-center text-sm">
                 끝없는 스크롤은 그만! 당신의 진정한 게임 취향을 알아보아요.
                 <br />
                 게임모리가 숨겨진 명작 인디게임 컬렉션의 세계를 발견하도록 도와드릴게요.
@@ -24,15 +29,11 @@ export default function Home() {
             {/* 테스트 시작하기 버튼 */}
             <ButtonStart
                 href="/personalize"
-                className="px-8 py-4 text-lg font-bold shadow-lg  hover:shadow-xl flex relative gap-1 mb-16"
+                className="flex items-center px-7 py-4 text-md font-bold shadow-lg hover:shadow-xl relative gap-1 mb-16"
             >
                 테스트 시작하기
-                <ArrowRightIcon />
+                <ArrowRightIcon size={24} />
             </ButtonStart>
-            {/* 3D CRT 모델 */}
-            {/* <div className="fixed w-96 h-96 m-auto -z-1 left-0">
-                <CrtModelViewer />
-            </div> */}
             {/* 흐르는 게임 썸네일들 */}
             <ThumbnailMarqueeList />
         </>
